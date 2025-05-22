704fd1b3ec418b13f14ea7f1abf511e138d9e593
abdul-rehmandev <abdulrehmandev001@gmail.com>
2025-05-22 14:56:10 +0300

    Deployement fixes

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -16,8 +16,7 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     lastName: {
-        type: String,
-        required: true
+        type: String
     },
     profilePicture: {
         type: String,
