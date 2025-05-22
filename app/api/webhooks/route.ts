eb225d7a0b816a16bfa91ec8ca735e1582e8d4cb
abdul-rehmandev <abdulrehmandev001@gmail.com>
2025-05-22 13:59:31 +0300

    Testing Database Integration

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,12 +1,30 @@
+import User from '@/lib/models/user.model';
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest, res: NextResponse) {
     try {
         const evt = await verifyWebhook(req)
 
         if (evt.type === 'user.created') {
-            console.log('userId:', evt.data.id)
+            const { first_name, last_name, image_url, email_addresses, id } = evt?.data;
+
+            const existingUser = await User.findOne({ email: email_addresses });
+
+            if (!existingUser) {
+                const newUser = new User({
+                    clerkId: id,
+                    email: email_addresses,
+                    firstName: first_name,
+                    lastName: last_name,
+                    profilePictue: image_url
+                });
+
+                await newUser.save();
+            }
+            else {
+                return NextResponse.json({ message: "Existing User SignedIn Successfully" })
+            }
         }
 
         return new Response('Webhook received', { status: 200 })
